bb70af6ddf667e2395713173a6e765b942eb7bbe
Jeewon Lee <jeewon.jlee@gmail.com>
2021-11-03 18:28:36 +0000

    Input module

--- client.js
+++ client.js
@@ -1,22 +1,26 @@
 const net = require("net");
 
-const connect = function () {
+// interpret incoming data as text
+const connect = function() {
   const conn = net.createConnection({
     host: '165.227.47.243',
     port: 50541
   });
 
-  // interpret incoming data as text
   conn.setEncoding("utf8");
 
   conn.on("connect", () => {
     console.log("Connection established");
+    const name = "LJW"
+    conn.write(`Name: ${name}`);
   });
 
-  const name = "LJW"
-  conn.write(`Name: ${name}`);
+  conn.on('data', (data) => {
+    console.log(data);
+  });
 
   return conn;
 };
 
-module.exports = connect;
+
+module.exports = { connect }
